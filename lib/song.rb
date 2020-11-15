010018073c34f2f30d6595467133be5e7a25f3e6
John Million <jpmillion@comcast.net>
2020-11-15 19:20:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 class Song 
   
   attr_reader :name, :artist, :genre 
-  @@count = 0
+  @@count = 0 
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1 
   end
   
 end
